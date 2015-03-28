0b4f10cdd3e8a4ebbe3f608eb7e3ca4cc845b10d
Lucian M <lucian.maran@outlook.com>
2015-03-28 21:06:50 +0200

    added mocha and travis

---
+++ server/test/sample1.js
@@ -0,0 +1,19 @@
+﻿var assert = require("assert");
+describe("Something", function () { 
+
+    describe("The way it smels", function () {
+        it("is pungent", function () { 
+            assert.ok(true);
+        });
+    });
+
+    describe('Array', function () {
+        describe('#indexOf()', function () {
+            it('should return -1 when the value is not present', function () {
+                assert.equal(-1, [1, 2, 3].indexOf(5));
+                assert.equal(-1, [1, 2, 3].indexOf(0));
+            })
+        })
+    })
+
+});
